7c63f7a41f57da48d7d34cfd6c1a619470718158
kasymbaevtimur <temuchi50@gmail.com>
2023-09-14 18:43:19 +0300

    Initial commit

---
+++ src/service/impl/MovieServiceImpl.java
@@ -0,0 +1,43 @@
+package service.impl;
+
+import dao.MovieDao;
+import model.Movie;
+import model.enums.Genre;
+import service.MovieService;
+
+import java.util.List;
+
+public class MovieServiceImpl implements MovieService {
+    private static final String PATH = "movie.json";
+    private MovieDao movieDao;
+
+    @Override
+    // TODO: 14.09.2023 вернуть фильм по ID
+    public Movie findById(int movieId) {
+        return null;
+    }
+
+    @Override
+    // TODO: 14.09.2023 вернуть фильм по названию
+    public Movie findByName(String name) {
+        return null;
+    }
+
+    @Override
+    // TODO: 14.09.2023 вернуть отсортированный лист по дате создание
+    public List<Movie> sortByDate() {
+        return null;
+    }
+
+    @Override
+    // TODO: 14.09.2023 отфилтрововать все фильмы по жанру
+    public List<Movie> filterByGenre(Genre genre) {
+        return null;
+    }
+
+    @Override
+    // TODO: 14.09.2023 найти обьект по ID и изменить его состояние
+    public void updateById(int movieId, Movie movie) {
+
+    }
+}
